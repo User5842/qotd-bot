29e63f01170759300b8327bfdab01126d1f0e850
Rafael Negron <r.negron1@umiami.edu>
2022-03-03 19:00:15 -0500

    Add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# QOTD Bot
+
+A simple Discord bot for the [leetcode_reddit](https://discord.gg/B45J8HKNth).
+
+Currently, the bot posts the [LeetCode](https://leetcode.com/problemset/all/) question of the day; every day at 0800.
--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const { Client, Intents } = require("discord.js");
+const cron = require("node-cron");
 
 const { token } = require("./config.json");
 const { QOTDClient } = require("./qotd.client");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.once("ready", async () => {
+client.once("ready", () => {
   const channel = client.channels.cache.find(
     (channel) => channel.name === "question-of-the-day"
   );
 
-  const {
-    activeDailyCodingChallengeQuestion: { link },
-  } = await QOTDClient.getQuestionOfTheDay();
+  cron.schedule("0 8 * * *", async () => {
+    const {
+      activeDailyCodingChallengeQuestion: { link },
+    } = await QOTDClient.getQuestionOfTheDay();
 
-  channel.send(`https://leetcode.com${link}`);
+    channel.send(`https://leetcode.com${link}`);
+  });
 });
 
 client.login(token);
